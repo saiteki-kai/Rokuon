0dee26775b0aa6e8649d470ce4555a0d33221007
Giuseppe <giuseppe.magazu@gmail.com>
2021-04-09 01:00:53 +0200

    Add recorder class

--- pulse_recorder.py
+++ pulse_recorder.py
@@ -5,66 +5,77 @@ import os
 import signal
 
 
-def load_record_module(sink):
-    cmd = [
-        "pactl",
-        "load-module",
-        "module-combine-sink",
-        "sink_name=record-audio",
-        f"slaves={sink}",
-        "sink_properties=device.description=record-audio",
-    ]
-    output = subprocess.run(cmd, stdout=subprocess.PIPE, check=True)
-    return int(output.stdout.strip())
+class Recorder:
+    def __init__(self):
+        self.subp = None
+        self.index = None
+        self.sink = None
+        self.record_module_id = None
 
+    def load_record_module(self, sink):
+        cmd = [
+            "pactl",
+            "load-module",
+            "module-combine-sink",
+            "sink_name=record-audio",
+            f"slaves={sink}",
+            "sink_properties=device.description=record-audio",
+        ]
+        output = subprocess.run(cmd, stdout=subprocess.PIPE, check=True)
+        return int(output.stdout.strip())
 
-def load_sink_inputs():
-    output = subprocess.run(
-        ["pacmd", "list-sink-inputs"], stdout=subprocess.PIPE, check=True
-    )
-    output = output.stdout.decode("utf-8")
+    def unload_record_module(self):
+        if self.record_module_id:
+            os.system(f"pactl unload-module {self.record_module_id}")
+            self.record_module_id = None
 
-    regex = re.compile(
-        r"index:\s+(\d+).*?sink:\s+(\d+)\s+<.*?>.*?client:\s+\d+\s+<(.*?)>.*?",
-        re.DOTALL | re.MULTILINE,
-    )
+    def load_sink_inputs(self):
+        output = subprocess.run(
+            ["pacmd", "list-sink-inputs"], stdout=subprocess.PIPE, check=True
+        )
+        output = output.stdout.decode("utf-8")
 
-    sinks = {}
-    clients = {}
+        regex = re.compile(
+            r"""index:\s+(\d+).*?
+                sink:\s+(\d+)\s+<.*?>.*?
+                client:\s+\d+\s+<(.*?)>.*?""",
+            re.DOTALL | re.MULTILINE | re.VERBOSE,
+        )
 
-    for match in regex.findall(output):
-        index = match[0]
-        sinks[index] = match[1]
-        clients[index] = match[2]
+        sinks = {}
+        clients = {}
 
-    return sinks, clients
+        for match in regex.findall(output):
+            index = match[0]
+            sinks[index] = match[1]
+            clients[index] = match[2]
 
+        return sinks, clients
 
-def record_start(index):
-    sinks, clients = load_sink_inputs()
-    sink = sinks[index]
-    record_module_id = load_record_module(sink)
-    os.system(f"pactl move-sink-input {index} record-audio")
+    def record_start(self, index):
+        sinks, clients = self.load_sink_inputs()
+        self.sink = sinks[index]
+        self.record_module_id = self.load_record_module(self.sink)
+        os.system(f"pactl move-sink-input {index} record-audio")
 
-    subp = subprocess.Popen(
-        """parec --format=s16le -d record-audio.monitor |  \
-        lame -r -q 3 --lowpass 17 --abr 192 - 'temp.mp3' > \
-        /dev/null &1>/dev/null""",
-        shell=True,
-    )
+        self.subp = subprocess.Popen(
+            """parec --format=s16le -d record-audio.monitor |  \
+            lame -r -q 3 --lowpass 17 --abr 192 - 'temp.mp3' > \
+            /dev/null &1>/dev/null""",
+            shell=True,
+        )
 
-    return {
-        "pid": os.getpgid(subp.pid),
-        "index": index,
-        "sink": sink,
-        "record_module_id": record_module_id,
-    }
+    def record_stop(self):
+        # Send CTRL+C to stop the recording
+        if (self.subp):
+            self.subp.send_signal(signal.SIGINT)
+            self.subp = None
 
+        # Restore old sink input
+        if self.index and self.sink:
+            os.system(f"pactl move-sink-input {self.index} {self.sink}")
+            self.index = None
+            self.sink = None
 
-def record_stop(data):
-    # Restore old sink input
-    os.system(f"pactl move-sink-input {data['index']} {data['sink']}")
-    os.system(f"pactl unload-module {data['record_module_id']}")
-
-    # Kill parec and lame to stop the recording
-    os.killpg(data["pid"], signal.SIGKILL)
+        # Unload record module
+        self.unload_record_module()
